ff13dd210f9f299ef9b7333bda5bd651a9e160a7
hweetae <taehwee_park@tmax.co.kr>
2023-06-26 21:47:21 +0900

    [New Feature] 회원가입 BE FE 연동 완료

--- recruitement-client/src/redux/configureStore.js
+++ recruitement-client/src/redux/configureStore.js
@@ -1,13 +1,18 @@
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import penderMiddleware from 'redux-pender';
 import modules from './modules';
 
-// Todo: 미들웨어 적용
+const isDevelopment = process.env.NODE_ENV === 'development';
+const composeEnhancers = isDevelopment
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+
+// Todo: 미들웨어, react-hot-loader 적용
 const configureStore = (initialState) => {
     const store = createStore(
         modules,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
+        initialState,
+        composeEnhancers(applyMiddleware(penderMiddleware()))
     );
 
     // hot-reloading 를 위한 코드
